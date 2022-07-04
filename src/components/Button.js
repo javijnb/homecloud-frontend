765809b39680f9fb8e8317fc84f23a541e44186a
Javier Núñez <javijnb@gmail.com>
2022-07-04 16:22:19 +0200

    feat: added button, header and task components

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+const Button = ({color, text, onClick}) => {
+
+  return (
+    <button 
+        onClick={onClick}
+        className='btn' 
+        style={{backgroundColor: color}}>
+            {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'green'
+}
+
+export default Button
