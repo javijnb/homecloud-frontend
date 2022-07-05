bd879f51d5f732dbcab89f871cea45a5cf670e63
Javier Núñez <javijnb@gmail.com>
2022-07-05 12:57:20 +0200

    feat: added mui components for login and toolbar components

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-const Toolbar = () => {
+export default function ButtonAppBar() {
   return (
-    <div>Toolbar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{backgroundColor: 'orange'}}>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1, textAlign: 'center'}}>
+            Homecloud
+          </Typography>
+          <Button color="inherit">Log out</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Toolbar
